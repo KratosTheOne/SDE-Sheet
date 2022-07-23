c9f1f089473637c78391c0dcdd420e7aa21dea9b
Kaustubh <kaustubhsinha2001@gmail.com>
2022-07-23 18:00:09 +0530

    commit

---
+++ Learn-Trees/Leetcode_InOrderTraversal(Iterative).cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+
+        stack<TreeNode*> st;
+        TreeNode *node = root;
+        vector<int> inOrder;
+
+        while(true)
+        {
+            if(node != NULL)
+            {
+                st.push(node);
+                node = node -> left;
+            }
+            else
+            {
+                if(st.empty() == true) break;
+                node = st.top();
+                st.pop();
+
+                inOrder.push_back(node -> val);
+
+                node = node -> right;
+            }
+        }
+
+        return inOrder;
+    }
+
+
+    /*
+    void traverseInOrder(TreeNode *curr, vector<int> &inOrder)
+    {
+        if(curr == NULL)
+            return;
+
+        traverseInOrder(curr -> left, inOrder);
+        inOrder.push_back(curr -> val);
+        traverseInOrder(curr -> right, inOrder);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+
+        vector<int> inOrder;
+
+        traverseInOrder(root, inOrder);
+
+        return inOrder;
+    }
+    */
+};
---
+++ Learn-Trees/Leetcode_PreOrderTraversal(Iterative).cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // Iterative
+
+    vector<int> preorderTraversal(TreeNode* root) {
+
+        vector<int> preOrder;
+
+        if(root == NULL) return preOrder;
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty())
+        {
+            root = st.top();
+            st.pop();
+            preOrder.push_back(root -> val);
+
+            if(root -> right != NULL)
+                st.push(root -> right);
+
+            if(root -> left != NULL)
+                st.push(root -> left);
+
+        }
+
+        return preOrder;
+    }
+
+    /* Recurive
+    void traversePreOrder(TreeNode * curr, vector<int> &preOrder)
+    {
+        if(curr == NULL)
+            return;
+
+        preOrder.push_back(curr -> val);
+        traversePreOrder(curr -> left, preOrder);
+        traversePreOrder(curr -> right, preOrder);
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+
+        vector<int> preOrder;
+
+        traversePreOrder(root, preOrder);
+
+        return preOrder;
+    }
+    */
+};
