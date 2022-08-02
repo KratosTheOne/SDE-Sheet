6c4683f9a63d8d3c21ff4a5944decba36fdcf9a8
Kaustubh <kaustubhsinha2001@gmail.com>
2022-08-02 22:47:42 +0530

    commit

---
+++ Day 3 - Arrays/Leetcode_Search-2D-Matrix-I.cpp
@@ -0,0 +1,25 @@
+//T.C & S.C -> O(n) & O(1)
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int n = matrix.size(), m = matrix[0].size();
+
+        int lo = 0, hi = n * m - 1;
+
+        while(lo <= hi)
+        {
+            int mid = (lo + (hi - lo) / 2);
+
+            if(matrix[mid / m][mid % m] == target)
+                return true;
+
+            if(matrix[mid / m][mid % m] < target)
+                lo = mid + 1;
+
+            else
+                hi = mid - 1;
+        }
+        return false;
+    }
+};
