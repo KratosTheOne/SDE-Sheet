2edb48eb67cd82f5c6d7347b208d7c6540dfec76
Kaustubh <kaustubhsinha2001@gmail.com>
2022-08-08 01:48:18 +0530

    commit

--- Day 11 - Binary Search/CodeStudio_Matrix-Median.cpp
+++ Day 11 - Binary Search/CodeStudio_Matrix-Median.cpp
@@ -1,1 +1,43 @@
+#include <bits/stdc++.h>
 
+//T.C & S.C -> O(32 * n * log(m)) & O(1)
+
+int countNumbersLessThanMid(vector<int> row, int mid)
+{
+    int l = 0, h = row.size() - 1;
+
+    while(l <= h)
+    {
+        int md = (l + h) >> 1;
+
+        if(row[md] <= mid)
+            l = md + 1;
+        else
+            h = md - 1;
+    }
+
+    return l;
+}
+
+int getMedian(vector<vector<int>> &matrix)
+{
+    int low = 0, high = 1e5;
+    int n = matrix.size(), m = matrix[0].size();
+
+    while(low <= high)
+    {
+        int mid = (low + high) >> 1;
+        int count = 0;
+        for(int i = 0; i < n; i++)
+        {
+            count += countNumbersLessThanMid(matrix[i], mid);
+        }
+
+        if(count <= (n * m) / 2)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return low;
+}
---
+++ Day 11 - Binary Search/Leetcode_Search-In-Rotated-Sorted-Array.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        //T.C & S.C -> O(logn) & O(1)
+
+        int low = 0, high = nums.size() - 1;
+        while(low <= high)
+        {
+            int mid = (low + high) >> 1;
+
+            if(nums[mid] == target)
+                return mid;
+
+            if(nums[low] <= nums[mid])
+            {
+                if(target >= nums[low] && target <= nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            else
+            {
+                if(target >= nums[mid] && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+
+        return -1;
+
+        /*
+        T.C & S.C -> O(n) & O(1)
+
+        int n = nums.size(), indi = 0;
+        bool flag = false;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] == target)
+            {
+                flag = true;
+                indi = i;
+                break;
+            }
+        }
+
+        return ((flag) ? (indi) : -1);
+        */
+    }
+};
